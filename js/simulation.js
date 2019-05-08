b54a132910c4adf5dd0ca38c1297f5300f66f754
minhanhn98 <minhanhn98@gmail.com>
2019-05-07 18:59:38 -0700

    attempting random video generator

--- js/simulation.js
+++ js/simulation.js
@@ -5,6 +5,7 @@ function main() {
   var powerbtn = document.getElementById('powerbtn');
   var shockbtn = document.getElementById('shockbtn');
   var blank = document.getElementById('blank');
+  var playing = document.getElementById('playing');
 
   // btn.style.color = "red";
   //btn.onclick = function() {myFunction2()};
@@ -35,7 +36,50 @@ function turnOnOff() {
       powerbtn.src="img/black-pw-btn.png";
       blank.style.display="block";
     }
+    randomGenerator();
     // btn.style.color = "blue";
+}
+
+function randomGenerator() {
+    var max = 5;
+    var min = 0;
+    var ranNum = Math.round(Math.random() * (+max - +min) + +min);
+    if(ranNum == 0){
+      console.log('ranNum = 0? '+ ranNum);
+      //MINIONS
+      playing.src="https://www.youtube.com/embed/SmbdY5FpRwA";
+      // playing.load();
+    }
+    else if(ranNum == 1){
+      console.log('ranNum = 1? '+ ranNum);
+      //ELMO
+      playing.src="https://www.youtube.com/embed/3gt6cHooI6M?rel=0 ";
+      // playing.load();
+    }
+    else if(ranNum == 2){
+      console.log('ranNum = 2? '+ ranNum);
+      // MINIONS
+      playing.src="https://www.youtube.com/embed/YPoTpvsFpvY";
+      // playing.load();
+    }
+    else if(ranNum == 3){
+      console.log('ranNum = 3? '+ ranNum);
+      //CORGIS
+      playing.src="https://www.youtube.com/embed/cS6J6BLDZoo";
+      // playing.load();
+    }
+    else if(ranNum == 4){
+      console.log('ranNum = 4? '+ ranNum);
+      //BOBS BURGERS
+      playing.src="https://www.youtube.com/embed/5zI82OxCpOw";
+      // playing.load();
+    }
+    else{
+      console.log('ranNum = 5? '+ ranNum);
+      // AED
+      playing.src="https://www.youtube.com/embed/0jaqSFCNrX0";
+    }
+    return ranNum;
 }
 
 // function hide(){
